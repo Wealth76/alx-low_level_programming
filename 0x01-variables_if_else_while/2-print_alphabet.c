171a269eaa334bbc2b3d281360246b5429f6be73
wealth76 <adetunjiolajide@gmail.com>
2022-10-14 18:36:21 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print different digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int data1, data2;
+
+	for (data1 = 0; data1 < 9; data++)
+	{
+		putchar((data1 % 10) + '0');
+		putchar((data2 % 10) + '0');
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+		putchar('\n');
+		return (0);
+}
