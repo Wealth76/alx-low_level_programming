efb23cda1567ef59e08f23c40171fc6dbb7979a8
wealth76 <adetunjiolajide@gmail.com>
2022-10-17 18:01:43 -0700

    7 print tebaph

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return: value 0 always
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+				c--;
+		}
+	putchar('\n');
+	return (0);
+}
