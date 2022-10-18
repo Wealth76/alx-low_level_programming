d3fe3b50ccf43dfafa538576af55154cdcbc52ca
wealth76 <adetunjiolajide@gmail.com>
2022-10-17 18:48:53 -0700

    100 print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ * main - entry value
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for
+		(a = 0);
+		(a < 100);
+		a++; {
+		putchar(a + '0');
+		if
+			(a < 99) {
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
