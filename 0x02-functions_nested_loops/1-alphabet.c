0e16ee77edb490c70e959f8b3e7eab825c5a2c9a
wealth76 <adetunjiolajide@gmail.com>
2022-10-18 15:10:50 -0700

    task 1 print lower-case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print lower-case alphabet
+ * a new line
+ */
+
+void print_alphabet(void);
+{
+	char Wealth;
+
+	for (Wealth = 'a'; la <= 'z'; Wealth++)
+		_putchar(Wealth);
+
+	_putchar('\n');
+}
