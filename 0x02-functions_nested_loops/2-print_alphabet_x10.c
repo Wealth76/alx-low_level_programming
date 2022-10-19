92459bbeb2922a90967f24562b815a3b1c6e3c11
wealth76 <adetunjiolajide@gmail.com>
2022-10-19 16:34:32 -0700

    task 2 print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabet 10 times at lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 0;
+
+	char wealth;
+
+	while (times++ <= 9)
+	{
+		for (wealth = 'a'; wealth <= 'z'; wealth++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
