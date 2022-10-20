1f5d83cc60bb9811814c1f89cd5e5b25ea11dfe7
wealth76 <adetunjiolajide@gmail.com>
2022-10-20 13:36:33 -0700

    infinite loop of c debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the infinite loop
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :\n)");
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i)
+	 * }
+	 */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
