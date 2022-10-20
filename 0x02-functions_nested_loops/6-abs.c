bb1ecbf893960dbe326921107393ea66e250199c
wealth76 <adetunjiolajide@gmail.com>
2022-10-19 17:51:11 -0700

    abs and int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - input the abs value of int
+ * @n: the int
+ * Return: as the abs of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
