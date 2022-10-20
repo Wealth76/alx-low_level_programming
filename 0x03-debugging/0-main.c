e96c11450f7b1a9fff8f4162abac2801c5ff378c
wealth76 <adetunjiolajide@gmail.com>
2022-10-20 06:13:06 -0700

    positive and negative debugging code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check for wrong input (positive or negative)
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+3 New c debugging task from alx course,
