ab8a098d6490b86e3de8fe1854dbe53e3238bfe2
wealth76 <adetunjiolajide@gmail.com>
2022-10-25 07:46:22 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,9 +6,10 @@ int _putchar(char c);
 void swap_int(int *a, int *b);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts2(char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
 
 #endif
