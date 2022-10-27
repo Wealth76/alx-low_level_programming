b0984abdd78d943b1c639d1e4bcc30abccbd2cd1
wealth76 <adetunjiolajide@gmail.com>
2022-10-27 16:14:32 -0700

    all for pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,11 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-ar *string_toupper(char *s);
+ar *string_toupper(char *);
 ar *cap_string(char *);
+char *leet(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif
