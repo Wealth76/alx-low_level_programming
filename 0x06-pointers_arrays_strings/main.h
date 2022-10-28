a2064f762b931c11f6889921f5792176983c426f
wealth76 <adetunjiolajide@gmail.com>
2022-10-28 15:00:50 -0700

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,6 @@ char *leet(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-char *rot13(char *str);
+char *rot13(char *s);
 
 #endif
