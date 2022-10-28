471370daea98d3a649aff80bba955f9c2ceaf8f6
wealth76 <adetunjiolajide@gmail.com>
2022-10-28 14:47:56 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,20 +9,21 @@
 
 char *rot13(char *s)
 {
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; *(s + i); i++)
+	while (*(s + count) != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		for (i = 0; i < 52; i++)
 		{
-			if (a[j] == *(s + i))
+			if (*(s + count) == alphabet[i])
 			{
-				*(s + i) = b[j];
+				*(s + count) = rot13[i];
 				break;
 			}
 		}
+		count++;
 	}
 	return (s)
 }
